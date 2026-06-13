a52ca96d3f0ab9a0df620cc748a59b07398c1797
iamsafal10 <i.am.safaltripathi@gmail.com>
2026-06-14 04:02:55 +0530

    cors fix

--- index.js
+++ index.js
@@ -11,12 +11,20 @@ const routes = require("./routes");
 dbConnect();
 app.use(
   cors({
-    origin: "https://foody-wine-three.vercel.app",
+    origin: ["https://foody-wine-three.vercel.app"],
     credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
+
+app.options(/.*/, cors());
 console.log("CORS LOADED");
 app.use(cookieParser());
 app.use(express.json());
+app.use((req, res, next) => {
+  console.log(req.method, req.url);
+  next();
+});
 app.use("/api", routes);
 app.listen(PORT, () => console.log(`Server has started at ${PORT}`));
