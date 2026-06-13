40d49a6858442bc7fb0c365c9fd64f091aba83aa
iamsafal10 <i.am.safaltripathi@gmail.com>
2026-06-14 03:49:23 +0530

    cors fix

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const path = require("path");
 const cors = require("cors");
 const dbConnect = require("./connection");
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 const cookieParser = require("cookie-parser");
 const routes = require("./routes");
 
@@ -15,6 +15,7 @@ app.use(
     credentials: true,
   })
 );
+console.log("CORS LOADED");
 app.use(cookieParser());
 app.use(express.json());
 app.use("/api", routes);
