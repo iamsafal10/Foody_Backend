eb0dd32989a905cbe6dc20ea15a643907b0e8a8b
iamsafal10 <i.am.safaltripathi@gmail.com>
2026-06-14 17:44:06 +0530

    fixes syntax erros

--- controllers/user.js
+++ controllers/user.js
@@ -70,12 +70,21 @@ async function handleUserLogin(req, res) {
 // USER LOGOUT
 async function handleUserLogOut(req, res) {
   try {
-    res.clearCookie("token").json({
+    res.clearCookie("token", {
+      httpOnly: true,
+      secure: true,
+      sameSite: "none",
+    });
+
+    return res.status(200).json({
       success: true,
       message: "Logged Out!",
     });
   } catch (error) {
-    return res.status(500).json({ success: false, message: error.message });
+    return res.status(500).json({
+      success: false,
+      message: error.message,
+    });
   }
 }
 // GET USER
